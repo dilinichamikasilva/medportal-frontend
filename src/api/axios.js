d5ba059c087d7d84f19d768562f368eed29fe25a
dilini <dilinichamikasilva@gmail.com>
2026-08-27 05:47:37 +0530

    feat: configure API base URL using environment variable

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://34.93.241.18:8080/api/v1',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://34.93.241.18:8080/api/v1',
 });
 
 export default API;
