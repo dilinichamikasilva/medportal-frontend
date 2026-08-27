1d1e739552f96434f75a7733a3caae0f756192d6
dilini <dilinichamikasilva@gmail.com>
2026-08-27 12:44:44 +0530

    fix: update API base URL in .env and axios configuration

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://136.110.213.76:80/api/v1',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://34.93.241.18:8080/api/v1',
 });
 
 export default API;
